74ceaf3286ca4b0472c1b327844ab0830331cf25
hydtechbloginc <81985743+hydtechbloginc@users.noreply.github.com>
2021-04-05 23:54:07 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "Z2hwX3NVZEpETmRET3huTWRUWGRoWDhhblpNNEoxb1FqQjBvem1GNg==",
+
+  // Your Github UserName
+  githubUserName: "hydtechbloginc",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog-github",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.9dda76533c313deb',
+  OAuthSecret: '02584638e2965d1eec5995c7d73de334ecf6b973',
+
+  // Your Personal Blog Title
+  title : "hyd tech blog" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "blog for fun",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
